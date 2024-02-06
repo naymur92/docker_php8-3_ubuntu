54d11cb834dae85f76d41a9aced7c64384c2512a
Naymur Rahman <naymurrahman@yahoo.com>
2024-02-06 17:52:29 +0600

    changed

--- public_html/phpinfo.php
+++ public_html/phpinfo.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP 8 - Ubuntu (phpInfo)</title>
+    <title>PHP 8.3 - Ubuntu (phpInfo)</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
 </head>
